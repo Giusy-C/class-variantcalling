cb3c953d8a854955013ce2f17a2ccabc2ec51e32
Giusy-C <giuseppina.carbone01@universitadipavia.it>
2025-02-09 18:59:56 +0100

    Update codiceprova.sh

--- codiceprova/codiceprova.sh
+++ codiceprova/codiceprova.sh
@@ -13,7 +13,7 @@ mkdir -p raw_data
 
 cd /config/workspace/datiesame
 
-tar -xzvf data_resequencing.tar.gz -C raw_data
+tar -xzvf data_resequencing.tar.gz -C /config/workspace/datiesame/analysis/raw_data
 
 #per ottenere il path corretto di raw_data cliccare con tasto destro su raw_data
 #al posto del comando sopra riportato possiamo fare solo tar -xzvf data_resequencing.tar.gz e poi spostare manualmente i campioni
